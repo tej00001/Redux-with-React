74920570d54a2a794bbc2e34317283d7a493ee71
tej00001 <tejachintu207@gmail.com>
2023-03-19 09:54:30 +0530

    Adding Task13Subscribing and Dispatching Actions

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,12 +1,36 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
 
+  const increamentHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decerement" });
+  };
+
+  const incrementBy5Handler = () => {
+    dispatch({ type: "INCREMENTBY5" });
+  };
+
+  const decrementBy5Handler = () => {
+    dispatch({ type: "DECREMENTBY5" });
+  };
+  const toggleCounterHandler = () => {};
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={increamentHandler}>Increament</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementBy5Handler}>IncrementBy5</button>
+        <button onClick={decrementBy5Handler}>DecremenentBy5</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/Store/storeDetails.js
+++ src/Store/storeDetails.js
@@ -1,34 +1,45 @@
-import {  legacy_createStore } from "redux";
+import { useReducer } from "react";
+import { legacy_createStore } from "redux";
 
-const counterReducer = (state ={counter:0},action) =>{
-    if(action.type === 'increment'){
-        return {
-            counter:state.counter + 1
-        }
-    }
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
 
-    if(action.type === 'decerement'){
-        return {
-            counter:state.counter - 1
-        }
-    }
+  if (action.type === "decerement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 
-    if(action.type === 'INCREMENTBY2'){
-        return {
-            counter:state.counter + 2
-        }
-    }
+  if (action.type === "INCREMENTBY2") {
+    return {
+      counter: state.counter + 2,
+    };
+  }
 
-    if(action.type === 'DECREMENTBY2'){
-        return {
-            counter:state.counter - 2
-        }
-    }
+  if (action.type === "DECREMENTBY2") {
+    return {
+      counter: state.counter - 2,
+    };
+  }
+  if (action.type === "INCREMENTBY5") {
+    return {
+      counter: state.counter + 5,
+    };
+  }
 
-    return state;
+  if (action.type === "DECREMENTBY5") {
+    return {
+      counter: state.counter - 5,
+    };
+  }
+  return state;
 };
 
-const store = legacy_createStore(counter);
+const store = legacy_createStore(counterReducer);
 // const CounterSubscriber=()=>{
 //     const Lateststate=store.getState();
 //     console.log(Lateststate)
